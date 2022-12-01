e04a3c7024c88bd2d2e6a1cd3c8c6cfaece6fba1
Laocc <fazo@qq.com>
2022-12-01 11:19:03 +0800

    edits

--- src/qr/qr_RawCode.php
+++ src/qr/qr_RawCode.php
@@ -95,16 +95,17 @@ class qr_RawCode
             if ($col >= $this->rsblocks[0]->dataLength) {
                 $row += $this->b1;
             }
-            $ret = $this->rsblocks[$row]->data[$col];
+            $ret = $this->rsblocks[$row]->data[intval($col)];
         } else if ($this->count < $this->dataLength + $this->eccLength) {
             $row = ($this->count - $this->dataLength) % $this->blocks;
             $col = ($this->count - $this->dataLength) / $this->blocks;
-            $ret = $this->rsblocks[$row]->ecc[$col];
+            $ret = $this->rsblocks[$row]->ecc[intval($col)];
         } else {
             return 0;
         }
         $this->count++;
-
         return $ret;
     }
+
+
 }
